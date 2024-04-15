f760fefe05a1740150b8764369f20879e9c17b3d
Maria Z <z0559882272@gmail.com>
2024-04-15 20:59:01 +0300

    xPath-start

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -1,22 +1,61 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class HomeWork {
 
     WebDriver wd;
 
     @BeforeClass
-    public void setUp(){
+    public void setUp() {
         wd = new ChromeDriver();
         wd.get("https://telranedu.web.app/login");
     }
 
 
     @Test
-    public void login(){
+    public void login() {
         //by tag name
+        WebElement div = wd.findElement(By.tagName("div"));
+        WebElement div1 = wd.findElement(By.cssSelector("div"));
+        WebElement div2 = wd.findElement(By.xpath("//div"));
+
+        WebElement h1 = wd.findElement(By.tagName("h1"));
+        WebElement h1_2 = wd.findElement(By.cssSelector("h1"));
+        WebElement h1_3 = wd.findElement(By.xpath("//h1"));
+
+
+        WebElement a = wd.findElement(By.tagName("a"));
+        WebElement a1 = wd.findElement(By.cssSelector("a"));
+        List<WebElement> list = wd.findElements(By.cssSelector("a"));
+
+        WebElement form = wd.findElement(By.tagName("form"));
+        WebElement form1 = wd.findElement(By.cssSelector("form"));
+
+        WebElement input = wd.findElement(By.tagName("input"));
+        WebElement input1 = wd.findElement(By.cssSelector("input"));
+
+        WebElement button = wd.findElement(By.tagName("button"));
+        WebElement button1 = wd.findElement(By.cssSelector("button"));
     }
+
+
+    //by class
+
+    //by id
+
+    //by attribute
+
+    //start
+
+    //end
+
+    //contains
+
 
 }
